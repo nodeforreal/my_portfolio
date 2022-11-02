11037a18ede293a3d354ef996d05a70708dff7e9
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-02 13:33:20 +0000

    refactor: Changed file and functinality names

---
+++ src/components/TagList.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+let list = [
+  "all",
+  "HTML",
+  "css",
+  "node.js",
+  "react.js",
+  "next.js",
+  "express",
+  "all",
+  "HTML",
+  "css",
+  "node.js",
+  "react.js",
+  "next.js",
+  "express",
+];
+
+const TagList = ({ className }) => {
+  const [currentTagList, setTagList] = useState("all");
+
+  return (
+    <Wrapper className={className}>
+      {list.map((label, index) => {
+        return (
+          <button
+            key={`${label}${index}`}
+            onClick={() => setTagList(label)}
+            className={`${
+              label === currentTagList ? "active" : ""
+            } category-btn`}
+          >
+            {label}
+          </button>
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+
+  .category-btn {
+    padding: 0.5rem 1rem;
+    border-radius: 1rem;
+    background-color: var(--primary);
+    font-size: var(--small-text);
+    font-weight: 600;
+    color: var(--secondary);
+    transition: var(--transition);
+    text-transform: capitalize;
+  }
+
+  .category-btn:hover {
+    background-color: var(--secondary);
+    color: var(--primary);
+  }
+
+  .active {
+    background-color: var(--secondary);
+    color: var(--primary);
+  }
+`;
+export default TagList;
