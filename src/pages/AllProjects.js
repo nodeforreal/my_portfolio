1a36d7dbd97ebda2059a25f4e5ac0fc333c0a634
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-02 13:10:14 +0000

    Create Allprojects.js

---
+++ src/pages/AllProjects.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const AllProjects = ()=>{
+    return <Wrapper>
+        all projects
+    </Wrapper>
+}
+
+const Wrapper = styled.section``
+
+export default AllProjects;
