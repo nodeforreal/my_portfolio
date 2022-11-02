dfdafe3029eb675125161d0dc376c8280e60bdcb
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-02 13:56:16 +0000

    feat: Added title, taglist, cards wrapper

--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -1,11 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { TagList, Card } from "../components";
 
-const AllProjects = ()=>{
-    return <Wrapper>
-        all projects
+const AllProjects = () => {
+  return (
+    <Wrapper className="full-page">
+      <h2 className="text-center app-section-title">all projects</h2>
+      <TagList />
+      <section className="project-cards">{}</section>
     </Wrapper>
-}
+  );
+};
 
-const Wrapper = styled.section``
+const Wrapper = styled.section``;
 
 export default AllProjects;
