248f3a79f4acbe8c4aff32f40c8b00dd9904bc39
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-02 14:49:04 +0000

    feat: Added projectsReducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "./features/ui/uiSlice";
+import projectsReducer from "./features/projects/projectsSlice";
 
 const store = configureStore({
-  reducer : {
-    ui: uiReducer
-  }
+  reducer: {
+    ui: uiReducer,
+    projects: projectsReducer,
+  },
 });
 
-export default store
+export default store;
