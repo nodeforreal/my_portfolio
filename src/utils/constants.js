701f7813c7e33795938422748da96f274125fceb
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-10 12:51:57 +0000

    refactor: var changed tagFilterList to filterTags

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ import { GrNode } from "react-icons/gr";
 import { FaSass, FaFigma, FaReact } from "react-icons/fa";
 import { DiMongodb } from "react-icons/di";
 
-export const tagFilterList = [
+export const filterTags = [
   {
     label: "Front-end",
     value: "front-end",
