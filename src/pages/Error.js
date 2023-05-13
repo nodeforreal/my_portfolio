dffb2fb3f97d379db23a989e9f90e91ea1f69b38
kaliyamoorthy <kaliyamoorthy@animaker.com>
2023-05-13 23:38:26 +0530

    KP-1 -page not found - page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Error = () => {
+
   return (
-    <div>Error</div>
+    <Wrapper className='grid-center'>
+      <h1>Page not found!!!</h1>
+      <div className='grid-center'>
+        <a href="/#home" className='secondary-btn btn'>Back</a>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  height: calc(100vh - 96px);
+`
 
 export default Error
